43fa8f1e0836e92c63b97020836810db7bbb4eb0
sadamseehar <sadamseehar@gmail.com>
2023-02-10 12:42:39 +0500

    created add to card button

--- store/controller/authview.py
+++ store/controller/authview.py
@@ -4,6 +4,9 @@ from django.contrib import messages
 
 from store.forms import CustomUserForm
 
+from django.contrib.auth import authenticate,login,logout
+
+
 def register(request):
     form = CustomUserForm()
     if request.method == 'POST':
@@ -12,6 +15,43 @@ def register(request):
             form.save()
             messages.success(request,"Succesfullt registerd")
             return redirect('/')
-            
+    
+    
     context = {'form':form}
     return render(request,"store/auth/register.html",context)
+
+
+
+def loginpage(request):
+    if request.user.is_authenticated:
+        messages.success(request,"already logged in ")
+        return redirect("/")
+
+    else :
+        if request.method == "POST":
+            name = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username = name , password = password)
+
+            if user is not None:
+                login(request, user)
+                messages.success(request,"login successfully")
+                return redirect("/")
+            else : 
+                messages.success(request,"incalid credentials")
+                return redirect('/login')
+            
+
+        return render(request,"store/auth/login.html")
+
+
+
+
+def logoutpage(request):
+    if request.user.is_authenticated:
+        logout(request)
+        messages.success(request,"logged out")
+        return redirect("/")
+
+    return redirect("/")
